02e8c40f63112b3abdabae47d499c36927887c8a
techsriman <tech.sriman.com>
2022-05-28 00:28:57 -0400

    rest

--- src/main/java/com/roadster/controller/PreferencesController.java
+++ src/main/java/com/roadster/controller/PreferencesController.java
@@ -1,20 +1,24 @@
 package com.roadster.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.roadster.config.Preferences;
 
-@Controller
+@RestController
 public class PreferencesController {
 	@Autowired
 	private Preferences preferences;
 
-	@GetMapping("/preferences")
-	public String showPreferences(ModelMap model) {
-		model.addAttribute("preferences", preferences);
-		return "preferences";
+	@GetMapping(value="/preferences",produces = {MediaType.APPLICATION_JSON_VALUE})
+	@ResponseBody
+	public Preferences showPreferences() {
+		//model.addAttribute("preferences", preferences);
+		return preferences;
 	}
 }
